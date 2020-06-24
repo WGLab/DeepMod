52653e581cf00f890c1f11859e55f01ee8f5a84a
Liu <liuq1@l-1-01.cm.cluster>
2020-06-23 20:29:25 -0400

    Improvement for setup

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setuptools.setup(
     packages=['DeepMod_scripts'],
     package_dir={'DeepMod_scripts': 'bin/DeepMod_scripts'},
     scripts=['bin/DeepMod.py', 'DeepMod_tools/*.py'],
-    #package_data={'': ['']},
-    data_files=[('train_deepmod', ['train_deepmod/*'])],
+    package_data={'train_deepmod': ['train_deepmod/*']},
+    #data_files=[('train_deepmod', ['train_deepmod/*'])],
     classifiers=[
         "Programming Language :: Python",
         'Intended Audience :: Science/Research',
